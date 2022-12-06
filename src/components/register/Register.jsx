a7c824d78c109aeb2a10a6122559a35310fac448
front_Dasha <64001891+Mango-Tango19@users.noreply.github.com>
2022-12-06 09:57:02 +0300

    add store

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -6,7 +6,7 @@ import {
 	faInfoCircle,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "./Register.scss";
+//import "./Register.scss";
 
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
