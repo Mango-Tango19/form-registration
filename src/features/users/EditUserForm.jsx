a10c08315bc71a49611fd69c5b78bf0f520e10fa
front_Dasha <64001891+Mango-Tango19@users.noreply.github.com>
2022-12-14 11:15:54 +0300

    add edit user form

--- src/features/users/EditUserForm.jsx
+++ src/features/users/EditUserForm.jsx
@@ -51,7 +51,7 @@ const EditUserForm = ({ user }) => {
 		}
 	}, [isSuccess, navigate, isSuccessDelUser]);
 
-	const onUserChanged = (e) => setUsername(e.target.value);
+	const onUserNameChanged = (e) => setUsername(e.target.value);
 
 	const onPasswordChanged = (e) => setPassword(e.target.value);
 
@@ -86,10 +86,109 @@ const EditUserForm = ({ user }) => {
 		await deleteUser(user.id);
 	};
 
-	const canSave =
-		[roles.length, username, password].every(Boolean) && !isLoading;
+	let canSave;
+	if (password) {
+		canSave =
+			[roles.length, validUsername, validPassword].every(Boolean) &&
+			!isLoading;
+	} else {
+		canSave = [roles.length, validUsername].every(Boolean) && !isLoading;
+	}
 
-	return <div>EditUserForm</div>;
+	const errClass = isError ? "errmsg" : "offscreen";
+	const validUserClass = !validUsername ? "form__input--incomplite" : "";
+	const validPwdClass = !validPassword ? "form__input--incomplite" : "";
+	const validRolesClass = !Boolean(roles.length)
+		? "form__input--incomplite"
+		: "";
+	const errContent = (error?.data?.message || delError?.data?.message) ?? "";
+
+	const content = (
+		<>
+			<p className={errClass}>{errContent}</p>
+
+			<form className='form' onSubmit={(e) => e.preventDefault()}>
+				<div className='form__title-row'>
+					<h2>Edit User</h2>
+					<div className='form__action-buttons'>
+						<button
+							className='icon-button'
+							title='Save'
+							onClick={onSaveUserClicked}
+							disabled={!canSave}
+						>
+							<FontAwesomeIcon icon={faSave} />
+						</button>
+						<button
+							className='icon-button'
+							title='Delete'
+							onClick={onDeleteUsersClick}
+						>
+							<FontAwesomeIcon icon={faTrashCan} />
+						</button>
+					</div>
+				</div>
+				<label className='form__label' htmlFor='username'>
+					Username: <span className='nowrap'>[3-20 letters]</span>
+				</label>
+				<input
+					className={`form__input ${validUserClass}`}
+					id='username'
+					name='username'
+					type='text'
+					autoComplete='off'
+					value={username}
+					onChange={onUserNameChanged}
+				/>
+
+				<label className='form__label' htmlFor='password'>
+					Password:{" "}
+					<span className='nowrap'>[empty = no change]</span>{" "}
+					<span className='nowrap'>[4-12 chars incl. !@#$%]</span>
+				</label>
+				<input
+					className={`form__input ${validPwdClass}`}
+					id='password'
+					name='password'
+					type='password'
+					value={password}
+					onChange={onPasswordChanged}
+				/>
+
+				<label
+					className='form__label form__checkbox-container'
+					htmlFor='user-active'
+				>
+					ACTIVE:
+					<input
+						className='form__checkbox'
+						id='user-active'
+						name='user-active'
+						type='checkbox'
+						checked={active}
+						onChange={onActiveChange}
+					/>
+				</label>
+
+				<label className='form__label' htmlFor='roles'>
+					ASSIGNED ROLES:
+				</label>
+				<select
+					id='roles'
+					name='roles'
+					className={`form__select ${validRolesClass}`}
+					multiple={true}
+					size='3'
+					value={roles}
+					onChange={onRolesChanged}
+				>
+					{options}
+				</select>
+			</form>
+		</>
+	);
+
+	return content;
 };
 
 export default EditUserForm;
